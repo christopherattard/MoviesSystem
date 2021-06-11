60fe3c62daffdb3e576d5a47c17a09384ea758ae
Chris <christopherattard@gmail.com>
2021-06-11 22:50:32 +0200

    Added new Xunit unit tests project.

---
+++ Movies.Tests/PreloadGrainStartupTaskTests.cs
@@ -0,0 +1,15 @@
+using System;
+using System.IO;
+using Xunit;
+
+namespace Movies.Tests
+{
+    public class PreloadGrainStartupTaskTests
+    {
+        [Fact]
+        public void JSONFile_FileExists_True()
+        {
+			Assert.True(File.Exists("C:\\Projects\\MoviesSystem\\movies.json"));
+        }
+    }
+}
